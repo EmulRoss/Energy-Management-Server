665671705888059d0fbdb83ee3fef138648800c4
Unknown <emul.ross@gmail.com>
2018-10-22 14:19:33 +0200

    Setup
    Initial setup of Flask
    Added JSON files that are sent via Flask on request

---
+++ Server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+app = Flask(__name__)
+cors = CORS(app)
+
+from flask import jsonify
+
+@app.route('/')
+def hello_world():
+	return 'Hello, World!'
+
+@app.route('/daily/heatmap/')
+def daily_heatmap():
+	file = open("GeoJSON.json","r")
+	return file.read()
+
+@app.route('/daily/<LocationID>')
+def daily_location(LocationID):
+	file = open("daily_" + LocationID + ".json","r")
+	return file.read()
